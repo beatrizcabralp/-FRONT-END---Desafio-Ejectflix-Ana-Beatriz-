283663b6d05d3392db94cba467e5441dbacf84ed
Ana Beatriz Cabral Pinto <beatrizcabralp.2002@gmail.com>
2024-10-03 20:27:58 -0300

    add modal

---
+++ assets/js/modal.js
@@ -0,0 +1,9 @@
+const openModal = () => {
+    const modal = document.getElementById("modalImage");
+    modal.style.display = "flex";
+  }
+  
+  const closeModal = () => {
+    const modal = document.getElementById("modalImage");
+    modal.style.display = "none";
+  }
